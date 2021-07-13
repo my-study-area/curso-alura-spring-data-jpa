d185ff42450d5f1774011942036528cceea38930
Adriano Avelino <adriano-st777@hotmail.com>
2021-07-12 21:45:44 -0300

    inicia controle de versão

---
+++ src/main/java/br/com/alura/spring/data/CursoAluraSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.spring.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoAluraSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoAluraSpringDataJpaApplication.class, args);
+	}
+
+}
